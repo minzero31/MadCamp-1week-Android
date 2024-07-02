83436f1b207404141ac96f449d30d38da1cef11a
minzero31 <77minyoung77@gmail.com>
2024-07-02 16:56:20 +0900

    test3

--- app/src/main/res/raw/contacts.js
+++ app/src/main/res/raw/contacts.js
@@ -1,7 +1,7 @@
 [
     {"name": "정형외과", "phoneNumber": "123-456-7890", "address": "경기도 뭐시기 뭐시기"},
     {"name": "소아과", "phoneNumber": "987-654-3210", "address": "경기도 뭐시기 뭐시기"},
-    {"name": "이비인후과", "phoneNumber": "555-666-7777", "address": "경기도 뭐시기 뭐시기"},
+    {"name": "이비인후과", "phgit oneNumber": "555-666-7777", "address": "경기도 뭐시기 뭐시기"},
     {"name": "대학병원", "phoneNumber": "555-666-7777","address": "경기도 뭐시기 뭐시기"},
     {"name": "내과", "phoneNumber": "555-666-7777", "address": "경기도 뭐시기 뭐시기"},
     {"name": "병원 1", "phoneNumber": "555-666-7777", "address": "경기도 뭐시기 뭐시기"},
@@ -9,7 +9,6 @@
     {"name": "병원 3", "phoneNumber": "555-666-7777", "address": "경기도 뭐시기 뭐시기"},
     {"name": "병원 4", "phoneNumber": "555-666-7777", "address": "경기도 뭐시기 뭐시기"},
     {"name": "병원 5", "phoneNumber": "555-666-7777", "address": "경기도 뭐시기 뭐시기"},
-    {"name": "병원 6", "phoneNumber": "555-666-7777", "address": "경기도 뭐시기 뭐시기"},
-    {"name": "병원 7", "phoneNumber": "555-666-7777", "address": "경기도 뭐시기 뭐시기"}
+
 
 ]
