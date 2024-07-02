4bf88b619f90aaa0dba4510fed1a14624f48d0a4
minzero31 <77minyoung77@gmail.com>
2024-07-02 19:56:25 +0900

    "Merge 후 강제 종료 오류 해결 완료 #20"

--- app/src/main/res/raw/contacts.js
+++ app/src/main/res/raw/contacts.js
@@ -1,14 +1,13 @@
 [
     {"name": "정형외과", "phoneNumber": "123-456-7890", "address": "경기도 뭐시기 뭐시기"},
     {"name": "소아과", "phoneNumber": "987-654-3210", "address": "경기도 뭐시기 뭐시기"},
-    {"name": "이비인후과", "phgit oneNumber": "555-666-7777", "address": "경기도 뭐시기 뭐시기"},
+    {"name": "이비인후과", "phoneNumber": "555-666-7777", "address": "경기도 뭐시기 뭐시기"},
     {"name": "대학병원", "phoneNumber": "555-666-7777","address": "경기도 뭐시기 뭐시기"},
     {"name": "내과", "phoneNumber": "555-666-7777", "address": "경기도 뭐시기 뭐시기"},
     {"name": "병원 1", "phoneNumber": "555-666-7777", "address": "경기도 뭐시기 뭐시기"},
     {"name": "병원 2", "phoneNumber": "555-666-7777", "address": "경기도 뭐시기 뭐시기"},
     {"name": "병원 3", "phoneNumber": "555-666-7777", "address": "경기도 뭐시기 뭐시기"},
     {"name": "병원 4", "phoneNumber": "555-666-7777", "address": "경기도 뭐시기 뭐시기"},
-    {"name": "병원 5", "phoneNumber": "555-666-7777", "address": "경기도 뭐시기 뭐시기"},
-
+    {"name": "병원 5", "phoneNumber": "555-666-7777", "address": "경기도 뭐시기 뭐시기"}
 
 ]
